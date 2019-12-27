5741bb4ed17ab0d1e1cbfd1357cb78d71f9a1df6
IgoreKMaN <y1876@mail.ru>
2019-12-27 13:57:56 +0300

    commit third

--- script/script.js
+++ script/script.js
@@ -1,8 +1,31 @@
-$( document ).ready(function() {
-   
-    $(".menu__mobile").click(function(){
-         console.log($(".mobile__list").height());
+$(document).ready(function () {
+
+    $(".menu__mobile").click(function () {
+        console.log($(".mobile__list").height());
         $(".menu__mobile").toggleClass("menu__active");
         $(".mobile").toggleClass("mobile__height");
     });
+
+
+/* */
+    $(function () {
+
+        var item__image = $(".item__image img");
+       
+        $(item__image).each(function (index, value) {
+
+            if ($(this).height() > 400) {
+                $(this).next().css( "top", "82%" )
+            }
+            
+            if ($(this).height() > 500) {
+                $(this).find(".share").css( "top", "88%" )
+            }
+            
+        });
+    });
+
+
+
+
 });
